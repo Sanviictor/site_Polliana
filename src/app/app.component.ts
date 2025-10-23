c6a8ea7c93faa91921a7e360016ea610a37d38d6
Victor Emanuel Galvao dos Santos <victor.egs@interno.saaeitauna.local>
2025-10-23 14:29:05 -0300

    Atualização da estrutura cabeçalho, separando-a em 2 partes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { HeroPageComponent } from "./componentes/hero-page/hero-page.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CabecalhoComponent],
+  imports: [CommonModule, CabecalhoComponent, HeroPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
