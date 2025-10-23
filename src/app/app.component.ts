3d11e16e82f345913142ffe32734e5e2ff73973c
Victor Emanuel Galvao dos Santos <victor.egs@interno.saaeitauna.local>
2025-10-23 17:25:33 -0300

    Adição das informações de serviço

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { HeroPageComponent } from "./componentes/hero-page/hero-page.component";
+import { ServicosComponent } from "./componentes/servicos/servicos.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CabecalhoComponent, HeroPageComponent],
+  imports: [CommonModule, CabecalhoComponent, HeroPageComponent, ServicosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
