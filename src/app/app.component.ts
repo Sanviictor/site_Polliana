0303a4cad98a2dc3cac4550f7d6ee340053969e6
Victor Emanuel Galvao dos Santos <victor.egs@interno.saaeitauna.local>
2025-10-24 14:50:02 -0300

    Adição do componente de contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { HeroPageComponent } from "./componentes/hero-page/hero-page.component";
 import { ServicosComponent } from "./componentes/servicos/servicos.component";
+import { PlanosComponent } from "./componentes/planos/planos.component";
+import { ContatoComponent } from "./componentes/contato/contato.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CabecalhoComponent, HeroPageComponent, ServicosComponent],
+  imports: [CommonModule, CabecalhoComponent, HeroPageComponent, ServicosComponent, PlanosComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
