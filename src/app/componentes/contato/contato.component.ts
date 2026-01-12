f033b4863e57b7acc7f6c5f4aca7b8449e48fdbf
Victor Emanuel Galvao dos Santos <victor.egs@interno.saaeitauna.local>
2026-01-12 09:39:16 -0300

    programacao do form e alguns reparos de design.

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
+  loading = false;
+  success = false;
+  error = false;
 
+  form = {
+    nome: '',
+    email: '',
+    telefone: '',
+    mensagem: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  enviarFormulario() {
+    this.loading = true;
+    this.success = false;
+    this.error = false;
+
+    this.http.post(
+      'https://formspree.io/f/xpqqzqke',
+      this.form
+    ).subscribe({
+      next: () => {
+        this.success = true;
+        this.loading = false;
+        this.form = {
+          nome: '',
+          email: '',
+          telefone: '',
+          mensagem: ''
+        };
+      },
+      error: () => {
+        this.error = true;
+        this.loading = false;
+      }
+    });
+  }
 }
